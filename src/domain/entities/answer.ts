614d37092778483f1887c41eecf7df69a464290a
danguya <daniel.yava16@gmail.com>
2024-04-05 11:07:33 +0100

    Mapeando propriedades

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,9 +1,12 @@
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityId } from "../../core/entities/unique-entity-id"
 
 interface AnswerProps {
+  authorId: UniqueEntityId
+  questionId: UniqueEntityId
   content: string
-  authorId: string
-  questionId: string
+  createdAt: Date
+  updatedAt?: Date
 }
 export class Answer extends Entity<AnswerProps> {
   get content() {
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,12 +1,15 @@
-import { randomUUID } from "crypto"
 import { Slug } from "./value-object/slug"
 import { Entity } from "../../core/entities/entity"
+import { UniqueEntityId } from "../../core/entities/unique-entity-id"
 
 interface QUestionProps {
+  authorId: UniqueEntityId
+  bestAnswerId?: UniqueEntityId
   title: string
   content: string
-  authorId: string
   slug: Slug
+  createdAt: Date
+  updatedAt?: Date
 }
 export class Question extends Entity<QUestionProps> {
 
